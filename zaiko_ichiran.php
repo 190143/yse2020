796363347fc3a21e5e48924d507c01151de3b84d
m25na <190174zo@yse-c.net>
2020-10-07 12:02:13 +0900

    7番かんりょう。データベースに取得

--- zaiko_ichiran.php
+++ zaiko_ichiran.php
@@ -34,6 +34,12 @@ if ($mysqli->connect_error) {
 $mysqli->set_charset('utf8');
 }
 //⑦書籍テーブルから書籍情報を取得するSQLを実行する。また実行結果を変数に保存する
+$sql = "SELECT * FROM books";
+if ($bookdate = $mysqli->query($sql)) {
+    // while ($row = $bookdate->fetch_assoc()) {
+    //     echo "{$row["id"]} {$row["name"]} {$row["email"]}" . PHP_EOL;
+    $result->close();
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
