9ff7c86227c1f39e4468444a76ba65a1336f8cb5
kasima318 <190157ol@yse-c.net>
2020-10-12 11:50:18 +0900

    ⑱⑲⑳㉑㉒㉓完了。 id/title/suthor/salesDate/price/stock表示

--- syukka.php
+++ syukka.php
@@ -106,12 +106,12 @@ function getId($id,$con){
 				?>
 				<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
 				<tr>
-					<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
-					<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
-					<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
+				<td><?php echo $getId_id["id"]/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
+					<td><?php echo $getId_id["title"]/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
+					<td><?php echo $getId_id["author"]/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
+					<td><?php echo $getId_id["salesDate"]/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
+					<td><?php echo $getId_id["price"]/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
+					<td><?php echo $getId_id["stock"]/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
 					<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
 				</tr>
 				<?php
