59cbd72461f63c46730206164ec671a1e02cab00
190143 <190143dl@yse-c.net>
2020-10-12 12:15:41 +0900

    バグ潰し

--- nyuka.php
+++ nyuka.php
@@ -14,16 +14,16 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (/* ①.の処理を行う */) {
-	//②セッションを開始する
-}
+// if (/* ①.の処理を行う */) {
+// 	//②セッションを開始する
+// }
 
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
-	//④SESSIONの「error2」に「ログインしてください」と設定する。
-	//⑤ログイン画面へ遷移する。
-}
+// if (/* ③の処理を書く */){
+// 	//④SESSIONの「error2」に「ログインしてください」と設定する。
+// 	//⑤ログイン画面へ遷移する。
+// }
 
 //⑥データベースへ接続し、接続情報を変数に保存する
 $host = 'localhost';
@@ -40,10 +40,10 @@ if ($mysqli->connect_error) {
 	$mysqli->set_charset('utf8');
 }
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-if(/* ⑧の処理を行う */){
-	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
-	//⑩在庫一覧画面へ遷移する。
-}
+// if(/* ⑧の処理を行う */){
+// 	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
+// 	//⑩在庫一覧画面へ遷移する。
+// }
 
 function getId($id,$con){
 	/* 
@@ -51,11 +51,14 @@ function getId($id,$con){
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
-	$sql = "SELECT * FROM books WHERE $id";
+	$sql = "SELECT * FROM books WHERE id = ".$id;
 	if ($bookdate = $con->query($sql)) {
-		$bookdate->close();
 		//⑫実行した結果から1レコード取得し、returnで値を返す。
 		return $bookdate;
+		/*
+		* close()場所移動願い
+		*/
+		$bookdate->close();
 	}
 	
 
@@ -93,9 +96,9 @@ function getId($id,$con){
 			 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 			 * 設定されていた場合はif文の中に入る。
 			 */ 
-			if(/* ⑬の処理を書く */){
-				//⑭SESSIONの「error」の中身を表示する。
-			}
+			// if(/* ⑬の処理を書く */){
+			// 	//⑭SESSIONの「error」の中身を表示する。
+			// }
 			?>
 			</div>
 			<div id="center">
@@ -117,7 +120,11 @@ function getId($id,$con){
 					 */
     				foreach($_POST['books'] as $book){
 						// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
-						$getId_id = getId($book,$mysqli)
+						// $getId_id = getId($book,$mysqli);
+						$getId_id = getId($book,$mysqli)->fetch_assoc();
+							// var_dump($row);
+						// }
+						// var_dump($getId_id["id"]);
 					?>
 					<input type="hidden" value="<?php echo $getId_id['id']/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
 					<tr>
