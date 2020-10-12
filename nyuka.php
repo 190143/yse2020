78253d703c304b6bce76daa86f5c2bc9933eddf4
190143 <190143dl@yse-c.net>
2020-10-12 11:23:39 +0900

    17番。戻り値からid設定

--- nyuka.php
+++ nyuka.php
@@ -119,7 +119,7 @@ function getId($id,$con){
 						// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
 						$getId_id = getId($book,$mysqli)
 					?>
-					<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
+					<input type="hidden" value="<?php echo $getId_id['id']/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
 					<tr>
 						<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
 						<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
