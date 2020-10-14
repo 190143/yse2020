87d98f566529162bdae6121fab739e95d504dd40
190143 <190143dl@yse-c.net>
2020-10-14 09:38:12 +0900

    SQL文修正

--- syukka.php
+++ syukka.php
@@ -49,7 +49,7 @@ function getId($id,$con){
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
-	$sql = "SELECT * FROM books WHERE $id";
+	$sql = "SELECT * FROM books WHERE id = ".$id;
 
 	// 変数呼び出し
 	$bookdate = null;
