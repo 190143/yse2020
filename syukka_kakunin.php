22bc26937d21c2a63b6f2d3980085f9cabc0da67
nisihiroto <190222xx@yse-c.net>
2020-10-19 11:25:11 +0900

    14~21変更

--- syukka_kakunin.php
+++ syukka_kakunin.php
@@ -73,7 +73,7 @@ foreach ($_post['books'] as $book){
 	// 	//⑬SESSIONの「error」に「数値以外が入力されています」と設定する。
 	$_SESSION['error'] = '数値以外が入力されています';
 	// 	//⑭「include」を使用して「syukka.php」を呼び出す。
-	include 'nyuka.php';
+	include('syukka.php');
 	// 	//⑮「exit」関数で処理を終了する。
 	exit;
 	 }
@@ -82,13 +82,17 @@ foreach ($_post['books'] as $book){
 	//⑯「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に⑪の処理で取得した値と⑧のDBの接続情報を渡す。
 	$book = getByID($book, $pdo);
 	//⑰ ⑯で取得した書籍の情報の「stock」と、⑩の変数を元にPOSTの「stock」から値を取り出して書籍情報の「stock」から値を引いた値を変数に保存する。
-
+	$book_total = $book_data_1['stock'] - $_POST['stock'][$book_quantity];
 	//⑱ ⑰の値が0未満か判定する。0未満の場合はif文の中に入る。
 // 	if(/* ⑱の処理を行う */){
+	if($book_total < 0){
 // 		//⑲SESSIONの「error」に「出荷する個数が在庫数を超えています」と設定する。
+$_SESSION['error'] = '出荷する個数が在庫数を超えています';
 // 		//⑳「include」を使用して「syukka.php」を呼び出す。
+include('syukka.php');
 // 		//㉑「exit」関数で処理を終了する。
-// 	}
+exit;
+}	
 	
 // 	//㉒ ⑩で宣言した変数をインクリメントで値を1増やす。
 $book_quantity ++;
