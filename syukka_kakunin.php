b7dba13f26c57308cc432e5d4373297c0351b3c1
m25na <190174zo@yse-c.net>
2020-10-19 12:31:40 +0900

    includeかっこ外し

--- syukka_kakunin.php
+++ syukka_kakunin.php
@@ -11,30 +11,32 @@
 //①セッションを開始する
 session_start();
 
-function getByid($id,$con){
+function getByid($id, $con)
+{
 	/* 
 	 * ②書籍を取得するSQLを作成する実行する。
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
-	$sql = "SELECT * FROM books WHERE id = ".$id;
+	$sql = "SELECT * FROM books WHERE id = " . $id;
 
 	// 変数呼び出し
 	$bookdate = null;
 	if ($bookdate = $con->query($sql)) {
-	//③実行した結果から1レコード取得し、returnで値を返す。
-	return $bookdate;
+		//③実行した結果から1レコード取得し、returnで値を返す。
+		return $bookdate;
 	}
 	$bookdate->close();
 }
 
-function updateByid($id,$con,$total){
+function updateByid($id, $con, $total)
+{
 	/*
 	 * ④書籍情報の在庫数を更新するSQLを実行する。
 	 * 引数で受け取った$totalの値で在庫数を上書く。
 	 * その際にWHERE句でメソッドの引数に$idに一致する書籍のみ取得する。
 	 */
-	$sql = "UPDATE books SET stock = ".$total."WHERE id = ".$id;
+	$sql = "UPDATE books SET stock = " . $total . "WHERE id = " . $id;
 	$con->query($sql);
 }
 
@@ -52,50 +54,50 @@ $password = '';
 $mysqli = new mysqli($host, $user_name, $password, $db_name);
 
 if ($mysqli->connect_error) {
-    echo $mysqli->connect_error;
-    exit();
+	echo $mysqli->connect_error;
+	exit();
 } else {
-//⑨データベースで使用する文字コードを「UTF8」にする
-$mysqli->set_charset('utf8');
-//⑩書籍数をカウントするための変数を宣言し、値を0で初期化する
-$book_quantity = 0;
-//⑪POSTの「books」から値を取得し、変数に設定する。
-foreach ($_POST['books'] as $book){
+	//⑨データベースで使用する文字コードを「UTF8」にする
+	$mysqli->set_charset('utf8');
+	//⑩書籍数をカウントするための変数を宣言し、値を0で初期化する
+	$book_quantity = 0;
+	//⑪POSTの「books」から値を取得し、変数に設定する。
+	foreach ($_POST['books'] as $book) {
 	/*
 	 * ⑫POSTの「stock」について⑩の変数の値を使用して値を取り出す。
 	 */
-	 $update_stock = $_POST['stock'][$book_quantity];
-	 /* 半角数字以外の文字が設定されていないかを「is_numeric」関数を使用して確認する。
+		$update_stock = $_POST['stock'][$book_quantity];
+		/* 半角数字以外の文字が設定されていないかを「is_numeric」関数を使用して確認する。
 	 * 半角数字以外の文字が入っていた場合はif文の中に入る。
 	 */
-	if (!is_numeric($update_stock)) {
-	// if (/* ⑫の処理を書く */) {
-	// 	//⑬SESSIONの「error」に「数値以外が入力されています」と設定する。
-	$_SESSION['error'] = '数値以外が入力されています';
-	// 	//⑭「include」を使用して「syukka.php」を呼び出す。
-	include('syukka.php');
-	// 	//⑮「exit」関数で処理を終了する。
-	exit;
-	 }
-}
+		if (!is_numeric($update_stock)) {
+			// if (/* ⑫の処理を書く */) {
+			// 	//⑬SESSIONの「error」に「数値以外が入力されています」と設定する。
+			$_SESSION['error'] = '数値以外が入力されています';
+			// 	//⑭「include」を使用して「syukka.php」を呼び出す。
+			include 'syukka.php';
+			// 	//⑮「exit」関数で処理を終了する。
+			exit;
+		}
+	}
 
 	//⑯「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に⑪の処理で取得した値と⑧のDBの接続情報を渡す。
 	$book_data_1 = getByid($book, $mysqli)->fetch_assoc();
 	//⑰ ⑯で取得した書籍の情報の「stock」と、⑩の変数を元にPOSTの「stock」から値を取り出して書籍情報の「stock」から値を引いた値を変数に保存する。
 	$book_total = $book_data_1['stock'] - $_POST['stock'][$book_quantity];
 	//⑱ ⑰の値が0未満か判定する。0未満の場合はif文の中に入る。
-// 	if(/* ⑱の処理を行う */){
-	if($book_total < 0){
-// 		//⑲SESSIONの「error」に「出荷する個数が在庫数を超えています」と設定する。
-$_SESSION['error'] = '出荷する個数が在庫数を超えています';
-// 		//⑳「include」を使用して「syukka.php」を呼び出す。
-include('syukka.php');
-// 		//㉑「exit」関数で処理を終了する。
-exit;
-}
+	// 	if(/* ⑱の処理を行う */){
+	if ($book_total < 0) {
+		// 		//⑲SESSIONの「error」に「出荷する個数が在庫数を超えています」と設定する。
+		$_SESSION['error'] = '出荷する個数が在庫数を超えています';
+		// 		//⑳「include」を使用して「syukka.php」を呼び出す。
+		include('syukka.php');
+		// 		//㉑「exit」関数で処理を終了する。
+		exit;
+	}
 
-// 	//㉒ ⑩で宣言した変数をインクリメントで値を1増やす。
-$book_quantity ++;
+	// 	//㉒ ⑩で宣言した変数をインクリメントで値を1増やす。
+	$book_quantity++;
 }
 
 /*
@@ -119,62 +121,65 @@ $book_quantity ++;
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-<meta charset="UTF-8">
-<title>出荷確認</title>
-<link rel="stylesheet" href="css/ichiran.css" type="text/css" />
+	<meta charset="UTF-8">
+	<title>出荷確認</title>
+	<link rel="stylesheet" href="css/ichiran.css" type="text/css" />
 </head>
+
 <body>
-<div id="header">
-	<h1>出荷確認</h1>
-</div>
-<form action="syukka_kakunin.php" method="post" id="test">
-	<div id="pagebody">
-		<div id="center">
-			<table>
-				<thead>
-					<tr>
-						<th id="book_name">書籍名</th>
-						<th id="stock">在庫数</th>
-						<th id="stock">出荷数</th>
-					</tr>
-				</thead>
-				<tbody>
-					<?php
-					//㉜書籍数をカウントするための変数を宣言し、値を0で初期化する。
-					$book_quantity = 0;
-					//㉝POSTの「books」から値を取得し、変数に設定する。
-					foreach ($_POST['books'] as $book){
-					// 	//㉞「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に㉜の処理で取得した値と⑧のDBの接続情報を渡す。
-					$book = getByID($book, $mysqli)->fetch_assoc();
-					 ?>
-					<tr>
-						<td><?php echo $book['title'];	/* ㉟ ㉞で取得した書籍情報からtitleを表示する。 */?></td>
-						<td><?php echo $book['stock'];	/* ㊱ ㉞で取得した書籍情報からstockを表示する。 */?></td>
-						<td><?php echo $_POST['stock'][$book_quantity]/* ㊲ POSTの「stock」に設定されている値を㉜の変数を使用して呼び出す。 */?></td>
-					</tr>
-					<input type="hidden" name="books[]" value="<?php echo $_POST['books'] /* ㊳ ㉝で取得した値を設定する */;?>">
-					<input type="hidden" name="stock[]" value='<?php echo $book = $_POST['stock']/* ㊴「POSTの「stock」に設定されている値を㉜の変数を使用して設定する。 */;?>'>
-					<?php
-					// 	//㊵ ㉜で宣言した変数をインクリメントで値を1増やす。
-					$book_quantity ++;
-					 }
-					?>
-				</tbody>
-			</table>
-			<div id="kakunin">
-				<p>
-					上記の書籍を出荷します。<br>
-					よろしいですか？
-				</p>
-				<button type="submit" id="message" formmethod="POST" name="add" value="ok">はい</button>
-				<button type="submit" id="message" formaction="syukka.php">いいえ</button>
+	<div id="header">
+		<h1>出荷確認</h1>
+	</div>
+	<form action="syukka_kakunin.php" method="post" id="test">
+		<div id="pagebody">
+			<div id="center">
+				<table>
+					<thead>
+						<tr>
+							<th id="book_name">書籍名</th>
+							<th id="stock">在庫数</th>
+							<th id="stock">出荷数</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						//㉜書籍数をカウントするための変数を宣言し、値を0で初期化する。
+						$book_quantity = 0;
+						//㉝POSTの「books」から値を取得し、変数に設定する。
+						foreach ($_POST['books'] as $book) {
+							// 	//㉞「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に㉜の処理で取得した値と⑧のDBの接続情報を渡す。
+							$book = getByID($book, $mysqli)->fetch_assoc();
+						?>
+							<tr>
+								<td><?php echo $book['title'];	/* ㉟ ㉞で取得した書籍情報からtitleを表示する。 */ ?></td>
+								<td><?php echo $book['stock'];	/* ㊱ ㉞で取得した書籍情報からstockを表示する。 */ ?></td>
+								<td><?php echo $_POST['stock'][$book_quantity]/* ㊲ POSTの「stock」に設定されている値を㉜の変数を使用して呼び出す。 */ ?></td>
+							</tr>
+							<input type="hidden" name="books[]" value="<?php echo $_POST['books'] /* ㊳ ㉝で取得した値を設定する */; ?>">
+							<input type="hidden" name="stock[]" value='<?php echo $book = $_POST['stock']/* ㊴「POSTの「stock」に設定されている値を㉜の変数を使用して設定する。 */; ?>'>
+						<?php
+							// 	//㊵ ㉜で宣言した変数をインクリメントで値を1増やす。
+							$book_quantity++;
+						}
+						?>
+					</tbody>
+				</table>
+				<div id="kakunin">
+					<p>
+						上記の書籍を出荷します。<br>
+						よろしいですか？
+					</p>
+					<button type="submit" id="message" formmethod="POST" name="add" value="ok">はい</button>
+					<button type="submit" id="message" formaction="syukka.php">いいえ</button>
+				</div>
 			</div>
 		</div>
+	</form>
+	<div id="footer">
+		<footer>株式会社アクロイト</footer>
 	</div>
-</form>
-<div id="footer">
-	<footer>株式会社アクロイト</footer>
-</div>
 </body>
+
 </html>
