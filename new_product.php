76ca0bfa5df2a9416af901df32a7d34110869eeb
190143 <190143dl@yse-c.net>
2020-10-28 11:22:07 +0900

    新規登録、PHP側

--- new_product.php
+++ new_product.php
@@ -1,3 +1,50 @@
+<?php
+/*
+ * session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
+ * 一致した場合はif文の中に入る。
+ */
+if ((function_exists('session_status')
+	&& session_status() !== PHP_SESSION_ACTIVE) || !session_id()) {
+	// 	//②セッションを開始する
+	session_start();
+}
+
+// //SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
+// if ($_SESSION['login'] == false) {
+// 	//③SESSIONの「error2」に「ログインしてください」と設定する。
+// 	$_SESSION['error2'] = 'ログインしてください';
+// 	//④ログイン画面へ遷移する。
+// 	header("Location: login.php");
+// }
+
+//データベースへ接続し、接続情報を変数に保存する
+$host = 'localhost';
+$user_name = 'zaiko2020_yse';
+$db_name = 'zaiko2020_yse';
+$password = '2020zaiko';
+$mysqli = new mysqli($host, $user_name, $password, $db_name);
+
+if ($mysqli->connect_error) {
+	echo $mysqli->connect_error;
+	exit();
+} else {
+	//⑦データベースで使用する文字コードを「UTF8」にする
+	$mysqli->set_charset('utf8');
+}
+
+// データーベースから最後のidを取得
+// SQL文
+$sql = " SELECT * FROM books ORDER BY id DESC LIMIT 1 ";
+// SQL実行
+$bookdate = $mysqli->query($sql);
+// 取得
+$getId_id = $bookdate->fetch_assoc();
+// id取得
+// var_dump($getId_id['id']);
+// 閉じる
+$bookdate->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
