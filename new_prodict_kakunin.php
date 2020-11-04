b93359cb3c282ed88808d2f96ec8e4cdcde24545
190143 <190143dl@yse-c.net>
2020-11-04 10:03:07 +0900

    HTML追加、hiddenから取得したidを表示、登録ボタン押されたらhiddenを使って送って..

--- new_prodict_kakunin.php
+++ new_prodict_kakunin.php
@@ -12,21 +12,20 @@ if ((function_exists('session_status')
 // データーベースに追加
 function new_prodict($con, $id, $title, $author, $salesDate, $price, $in_stock)
 {
+
     // SQL文
-    $sql = "INSERT INTO books (id, title, author, salesDate, isbn, price, stock) VALUES (" . $id . " , " . $title . " , " . $author . " ," . $salesDate . " , 0 , " . $price . " , " . $in_stock . ")";
-    var_dump($sql);
-    // $con->query($sql);
+    $sql = "INSERT INTO books (id, title, author, salesDate, isbn, price, stock) VALUES (" . $id . " , '" . $title . "' , '" . $author . "' ," . $salesDate . " , 0 , " . $price . " , " . $in_stock . ")";
+    // var_dump($sql);
+    $con->query($sql);
 }
 
 // //SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-// if ($_SESSION['login'] == false) {
-// 	//③SESSIONの「error2」に「ログインしてください」と設定する。
-// 	$_SESSION['error2'] = 'ログインしてください';
-// 	//④ログイン画面へ遷移する。
-// 	header("Location: login.php");
-// }
-var_dump($_POST['id']);
-// var_dump($_POST['id'], $_POST['title'], $_POST['author'], $_POST['salesDate'], $_POST['price'], $_POST['in_stock']);
+if ($_SESSION['login'] == false) {
+	//③SESSIONの「error2」に「ログインしてください」と設定する。
+	$_SESSION['error2'] = 'ログインしてください';
+	//④ログイン画面へ遷移する。
+	header("Location: login.php");
+}
 
 //データベースへ接続し、接続情報を変数に保存する
 $host = 'localhost';
@@ -63,11 +62,73 @@ if (!empty($_POST['add'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>新商品追加確認</title>
+    <link rel="stylesheet" href="css/ichiran.css" type="text/css" />
 </head>
 
 <body>
-
+    <!-- ヘッダ -->
+    <div id="header">
+        <h1>新商品追加確認</h1>
+    </div>
+    <!-- メニュー -->
+    <div id="menu">
+        <nav>
+            <ul>
+                <li><a href="zaiko_ichiran.php?page=1">追加書籍</a></li>
+            </ul>
+        </nav>
+    </div>
+    <form action="new_prodict_kakunin.php" method="post">
+        <div id="pagebody">
+            <!-- エラーメッセージ -->
+            <div id="error">
+            </div>
+            <div id="center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th id="id">ID</th>
+                            <th id="book_name">書籍名</th>
+                            <th id="author">著者名</th>
+                            <th id="salesDate">発売日</th>
+                            <th id="itemPrice">金額(円)</th>
+                            <th id="stock">在庫数</th>
+                            <th id="in">入荷数</th>
+                        </tr>
+                    </thead>
+                    <!-- hiddenとは...「Webブラウザに表示されない文字をPOST/GETで送ることができる便利な関数である」 -->
+                    <input type="hidden" value="<?php echo $_POST['id']; ?>" name="id">
+                    <input type="hidden" value="<?php echo $_POST['title']; ?>" name="title">
+                    <input type="hidden" value="<?php echo $_POST['author']; ?>" name="author">
+                    <input type="hidden" value="<?php echo $_POST['salesDate']; ?>" name="salesDate">
+                    <input type="hidden" value="<?php echo $_POST['price']; ?>" name="price">
+                    <input type="hidden" value="<?php echo $_POST['stock']; ?>" name="stock">
+                    <input type="hidden" value="<?php echo $_POST['in_stock']; ?>" name="in_stock">
+                    <tr>
+                        <td><?php echo $_POST['id']; ?></td>
+                        <td><?php echo $_POST['title']; ?></td>
+                        <td><?php echo $_POST['author']; ?></td>
+                        <td><?php echo $_POST['salesDate']; ?></td>
+                        <td><?php echo $_POST['price']; ?></td>
+                        <td><?php echo $_POST['stock']; ?></td>
+                        <td><?php echo $_POST['in_stock']; ?></td>
+                    </tr>
+                </table>
+                <div id="kakunin">
+                    <p>
+                        上記の書籍を登録します。<br>
+                        よろしいですか？
+                    </p>
+                    <button type="submit" id="message" formmethod="POST" name="add" value="ok">はい</button>
+                    <button type="submit" id="message" formaction="new_product.php">いいえ</button>
+                </div>
+            </div>
+        </div>
+        <!-- フッター -->
+        <div id="footer">
+            <footer>株式会社アクロイト</footer>
+        </div>
 </body>
 
 </html>
