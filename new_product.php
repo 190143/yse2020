1b2d7fbb3e380cd466e2a9dffe1fd62e0f2fb57e
190143 <190143dl@yse-c.net>
2020-11-04 09:59:45 +0900

    ログインチェック有効化

--- new_product.php
+++ new_product.php
@@ -10,12 +10,12 @@ if ((function_exists('session_status')
 }
 
 // //SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-// if ($_SESSION['login'] == false) {
-// 	//③SESSIONの「error2」に「ログインしてください」と設定する。
-// 	$_SESSION['error2'] = 'ログインしてください';
-// 	//④ログイン画面へ遷移する。
-// 	header("Location: login.php");
-// }
+if ($_SESSION['login'] == false) {
+	//③SESSIONの「error2」に「ログインしてください」と設定する。
+	$_SESSION['error2'] = 'ログインしてください';
+	//④ログイン画面へ遷移する。
+	header("Location: login.php");
+}
 
 //データベースへ接続し、接続情報を変数に保存する
 $host = 'localhost';
